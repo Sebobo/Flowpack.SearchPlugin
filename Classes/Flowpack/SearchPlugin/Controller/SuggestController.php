0c7ad9bfbff2c259691b579328d8e1e62f668f35
Sebastian Helzle <sebastian@helzle.it>
2018-02-17 17:40:42 +0100

    TASK: Improve suggestion performance and catch errors with invalid characters
    This is an adaption of the Neos 3.* compatible SearchPlugin code.

--- Classes/Flowpack/SearchPlugin/Controller/SuggestController.php
+++ Classes/Flowpack/SearchPlugin/Controller/SuggestController.php
@@ -12,21 +12,36 @@ namespace Flowpack\SearchPlugin\Controller;
  */
 
 use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Eel\ElasticSearchQueryBuilder;
-use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Eel\ElasticSearchQueryResult;
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\ElasticSearchClient;
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Exception\QueryBuildingException;
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Cache\Frontend\VariableFrontend;
 use TYPO3\Flow\Mvc\Controller\ActionController;
-use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+use TYPO3\Neos\Controller\CreateContentContextTrait;
 
 /**
  * Class SuggestController
  */
 class SuggestController extends ActionController
 {
+    use CreateContentContextTrait;
+
+    /**
+     * @var ElasticSearchClient
+     */
+    protected $elasticSearchClient;
+
     /**
      * @Flow\Inject
      * @var ElasticSearchQueryBuilder
      */
     protected $elasticSearchQueryBuilder;
+
+    /**
+     * @Flow\Inject
+     * @var VariableFrontend
+     */
+    protected $elasticSearchQueryTemplateCache;
 
     /**
      * @var array
@@ -35,59 +50,142 @@ class SuggestController extends ActionController
         'json' => 'TYPO3\Flow\Mvc\View\JsonView'
     ];
 
-    /**
-     * @param NodeInterface $contextNode
-     * @param string $term
-     */
-    public function indexAction(NodeInterface $contextNode, $term)
+    public function initializeObject()
     {
-        /** @var ElasticSearchQueryBuilder $query */
-        $query = $this->elasticSearchQueryBuilder->query($contextNode);
-        /** @var ElasticSearchQueryResult $result */
-        $result = $query
-            ->queryFilter('prefix', [
-                '__completion' => $term
-            ])
-            ->limit(0)
-            ->aggregation('autocomplete', [
-                'terms' => [
-                    'field' => '__completion',
-                    'order' => [
-                        '_count' => 'desc'
-                    ],
-                    'include' => [
-                        'pattern' => $term . '.*'
-                    ]
-                ]
-            ])
-            ->suggestions('suggestions', [
-                'text' => $term,
-                'completion' => [
-                    'field' => '__suggestions',
-                    'fuzzy' => true,
-                    'context' => [
-                        'parentPath' => $contextNode->getPath(),
-                        'workspace' => array_unique(['live', $contextNode->getContext()->getWorkspace()->getName()]),
-                        'dimensionCombinationHash' => md5(json_encode($contextNode->getContext()->getDimensions())),
-                    ]
-                ]
-            ])
-            ->execute();
+        if ($this->objectManager->isRegistered(ElasticSearchClient::class)) {
+            $this->elasticSearchClient = $this->objectManager->get(ElasticSearchClient::class);
+            $this->elasticSearchQueryBuilder = $this->objectManager->get(ElasticSearchQueryBuilder::class);
+        }
+    }
 
-        $aggregations = $result->getAggregations();
-        $aggregationOptions = $aggregations['autocomplete']['buckets'];
-
-        if (count($aggregationOptions) > 0) {
-            $options = array_map(function ($option) {
-                return $option['key'];
-            }, $aggregationOptions);
-        } else {
-            $suggestions = $result->getSuggestions();
-            $options = array_map(function ($option) {
-                return $option['text'];
-            }, $suggestions['options']);
+    /**
+     * @param string $contextNodeIdentifier
+     * @param string $dimensionCombination
+     * @param string $term
+     * @throws QueryBuildingException
+     */
+    public function indexAction($contextNodeIdentifier, $dimensionCombination, $term)
+    {
+        if ($this->elasticSearchClient === null) {
+            throw new \RuntimeException('The SuggestController needs an ElasticSearchClient, it seems you run without the flowpack/elasticsearch-contentrepositoryadaptor package, though.', 1487189823);
         }
 
-        $this->view->assign('value', $options);
+        $result = [
+            'completions' => [],
+            'suggestions' => []
+        ];
+
+        if (!is_string($term)) {
+            $result['errors'] = ['term has to be a string'];
+            $this->view->assign('value', $result);
+            return;
+        }
+
+        $requestJson = $this->buildRequestForTerm($contextNodeIdentifier, $dimensionCombination, $term);
+
+        try {
+            $response = $this->elasticSearchClient->getIndex()->request('POST', '/_search', [], $requestJson)->getTreatedContent();
+            $result['completions'] = $this->extractCompletions($response);
+            $result['suggestions'] = $this->extractSuggestions($response);
+        } catch (\Exception $e) {
+            $result['errors'] = ['Could not execute query'];
+        }
+
+        $options = $result['completions'];
+        if (empty($options)) {
+            $options = $result['suggestions'];
+        }
+
+        $options = array_map(function ($option) {
+            return (is_array($option) && array_key_exists('text', $option)) ? $option['text'] : $option;
+        }, $options);
+
+        $this->view->assign('value', array_filter($options));
+    }
+
+    /**
+     * @param string $term
+     * @param string $contextNodeIdentifier
+     * @param string $dimensionCombination
+     * @return ElasticSearchQueryBuilder
+     * @throws QueryBuildingException
+     */
+    protected function buildRequestForTerm($contextNodeIdentifier, $dimensionCombination, $term)
+    {
+        $cacheKey = $contextNodeIdentifier . '-' . md5($dimensionCombination);
+        $termPlaceholder = '---term-soh2gufuNi---';
+        $term = strtolower($term);
+        // The suggest function only works well with one word
+        // and the term is trimmed to alnum characters to avoid errors
+        $suggestTerm = preg_replace('/[[:^alnum:]]/', '', explode(' ', $term)[0]);
+        if (!$this->elasticSearchQueryTemplateCache->has($cacheKey)) {
+            $contentContext = $this->createContentContext('live', json_decode($dimensionCombination, true));
+            $contextNode = $contentContext->getNodeByIdentifier($contextNodeIdentifier);
+            /** @var ElasticSearchQueryBuilder $query */
+            $query = $this->elasticSearchQueryBuilder->query($contextNode);
+            $query
+                ->queryFilter('prefix', [
+                    '__completion' => $termPlaceholder
+                ])
+                ->limit(1)
+                ->aggregation('autocomplete', [
+                    'terms' => [
+                        'field' => '__completion',
+                        'order' => [
+                            '_count' => 'desc'
+                        ],
+                        'include' => [
+                            'pattern' => $termPlaceholder . '.*'
+                        ]
+                    ]
+                ])
+                ->suggestions('suggestions', [
+                    'text' => $termPlaceholder,
+                    'completion' => [
+                        'field' => '__suggestions',
+                        'fuzzy' => true,
+                        'size' => 10,
+                        'context' => [
+                            'parentPath' => $contextNode->getPath(),
+                            'workspace' => 'live',
+                            'dimensionCombinationHash' => md5(json_encode($contextNode->getContext()->getDimensions())),
+                        ]
+                    ]
+                ]);
+            $requestTemplate = json_encode($query->getRequest());
+            $this->elasticSearchQueryTemplateCache->set($contextNodeIdentifier, $requestTemplate);
+        } else {
+            $requestTemplate = $this->elasticSearchQueryTemplateCache->get($cacheKey);
+        }
+        return str_replace($termPlaceholder, $suggestTerm, $requestTemplate);
+    }
+
+    /**
+     * Extract autocomplete options
+     *
+     * @param $response
+     * @return array
+     */
+    protected function extractCompletions($response)
+    {
+        $aggregations = isset($response['aggregations']) ? $response['aggregations'] : [];
+        return array_map(function ($option) {
+            return $option['key'];
+        }, $aggregations['autocomplete']['buckets']);
+    }
+
+    /**
+     * Extract suggestion options
+     *
+     * @param $response
+     * @return array
+     */
+    protected function extractSuggestions($response)
+    {
+        $suggestionOptions = isset($response['suggest']) ? $response['suggest'] : [];
+        if (count($suggestionOptions['suggestions'][0]['options']) > 0) {
+            return $suggestionOptions['suggestions'][0]['options'];
+        }
+        return [];
     }
 }
